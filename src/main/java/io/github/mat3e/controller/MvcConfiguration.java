f4ba05154cb52460eec51208652119578b1543b6
Arleta Madej <arleta.madej@gmail.com>
2024-03-05 16:03:34 +0100

    wstrzykiwanie kolekcji interceptorów

--- src/main/java/io/github/mat3e/controller/MvcConfiguration.java
+++ src/main/java/io/github/mat3e/controller/MvcConfiguration.java
@@ -1,14 +1,24 @@
 package io.github.mat3e.controller;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Set;
+
 @Configuration
 public class MvcConfiguration implements WebMvcConfigurer {
+    private final Set<HandlerInterceptor> interceptors;
+
+    MvcConfiguration(final Set<HandlerInterceptor> interceptors) {
+        this.interceptors = interceptors;
+    }
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new LoggerInterceptor());
-        WebMvcConfigurer.super.addInterceptors(registry);
+        interceptors.forEach(registry::addInterceptor);
+//        registry.addInterceptor(new LoggerInterceptor());
+//        WebMvcConfigurer.super.addInterceptors(registry);
     }
 }
