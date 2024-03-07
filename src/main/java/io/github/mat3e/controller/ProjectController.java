b2a2e4a0f2311a1ea5a3953eff558ab365f33dfd
Arleta Madej <arleta.madej@gmail.com>
2024-03-07 17:24:24 +0100

    zapis przez formularz

--- src/main/java/io/github/mat3e/controller/ProjectController.java
+++ src/main/java/io/github/mat3e/controller/ProjectController.java
@@ -1,18 +1,41 @@
 package io.github.mat3e.controller;
 
+import io.github.mat3e.logic.ProjectService;
+import io.github.mat3e.model.ProjectStep;
 import io.github.mat3e.model.projection.ProjectWriteModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/projects")
 public class ProjectController {
+    private final ProjectService service;
+
+    ProjectController(final ProjectService service) {
+        this.service = service;
+    }
 
     @GetMapping
     String showProjects(Model model){
         model.addAttribute("project", new ProjectWriteModel());
+        return "projects";
+    }
+
+    @PostMapping
+    String addProject(@ModelAttribute("project") ProjectWriteModel current, Model model ){
+        service.save(current);
+        model.addAttribute("project", new ProjectWriteModel());
+        model.addAttribute("message","Dodano projekt");
+        return "projects";
+    }
+
+    @PostMapping(params="addStep")
+    String addProjectStep(@ModelAttribute("project") ProjectWriteModel current){
+        current.getSteps().add(new ProjectStep());
         return "projects";
     }
 
