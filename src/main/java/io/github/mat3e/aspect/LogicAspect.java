4155e663e4edd5db283335972feed389fb285025
Arleta Madej <arleta.madej@gmail.com>
2024-03-11 00:29:03 +0100

    @Pointcut and @Before

--- src/main/java/io/github/mat3e/aspect/LogicAspect.java
+++ src/main/java/io/github/mat3e/aspect/LogicAspect.java
@@ -2,21 +2,37 @@ package io.github.mat3e.aspect;
 
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.Timer;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LogicAspect {
+    private static final Logger logger = LoggerFactory.getLogger(LogicAspect.class);
     private final Timer timer;
 
     LogicAspect(MeterRegistry registry) {
         this.timer = registry.timer("logic.product.create.group");
     }
 
-    @Around("execution(* io.github.mat3e.logic.ProjectService.createGroup(..))")
+
+    @Pointcut("execution(* io.github.mat3e.logic.ProjectService.createGroup(..))")
+    void projectServiceCreateGroup(){
+    }
+
+    @Before("projectServiceCreateGroup()")
+    void logMethodCall(JoinPoint jp){
+        logger.info("Before {} with {}", jp.getSignature().getName(), jp.getArgs());
+    }
+
+    @Around("projectServiceCreateGroup()")
     Object aroundProjectCreateGroup(ProceedingJoinPoint jp) {
         return timer.record(() -> {
             try {
